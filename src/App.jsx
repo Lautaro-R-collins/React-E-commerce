fea0f8f7cdfa59c3f0ce9a014ab563c7898485a0
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-24 16:52:33 -0300

    Add - home in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../src/layout/layout.jsx";
 import "./App.css";
+
+import { Home } from "./pages/Home.jsx";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
