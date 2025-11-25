4e1512f2db6523c37621d0a74100754732703bc8
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-25 02:11:12 -0300

    act - app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "../src/layout/layout.jsx";
 import "./App.css";
 
+// Routes pages
 import { Home } from "./pages/Home.jsx";
+import { Login } from "./pages/Login.jsx";
+import { Register } from "./pages/Register.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Route>
     </Routes>
   );
