be1980792bbba3127e141386c3e2028d247d2d84
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-25 02:10:40 -0300

    add register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "../components/register/registerForm.jsx";
+
+export const Register = () => {
+  return (
+    <div className=" text-center my-10">
+      <RegisterForm />
+    </div>
+  );
+};
