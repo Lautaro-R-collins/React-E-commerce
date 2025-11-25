9530a2e6a67800b57a57366373c7d4219a074cf4
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-25 02:10:02 -0300

    add - login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "../components/login/loginForm.jsx";
+
+export const Login = () => {
+  return (
+    <div className="text-center my-10">
+      <LoginForm />
+    </div>
+  );
+};
