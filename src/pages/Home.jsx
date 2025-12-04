98eb3163afd51ed2a1ead28ea71d93d85e66f1f8
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-03 23:01:09 -0300

    renderizado home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import { useProduct } from "../context/productContext.jsx";
+import CardProduct from "../components/products/CardProduct.jsx";
 
 export const Home = () => {
-  return <div className="text-3xl text-center font-bold">bienvenido a la tienda</div>;
+  const { products, loading, error } = useProduct();
 
+  // Loading
+  if (loading) {
+    return (
+      <div className="text-center mt-10 text-xl">
+        Cargando productos...
+      </div>
+    );
+  }
+
+  // Error
+  if (error) {
+    return (
+      <div className="text-center mt-10 text-red-500 font-semibold">
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <div className="px-4">
+      <h1 className="text-3xl text-center font-bold my-6">
+        Bienvenido a la tienda
+      </h1>
+
+      {/* GRID de productos */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center">
+        {products.map((product) => (
+          <CardProduct key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
