ed82c8a44a3d3d7cdd31c6c9bd61491094a00848
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-06 19:58:53 -0300

    ref - form

--- src/components/register/registerForm.jsx
+++ src/components/register/registerForm.jsx
@@ -42,132 +42,136 @@ export const RegisterForm = () => {
   }
 
   return (
-    <div className="shadow-2xl p-5 rounded-2xl">
-      <h1 className="text-xl md:text-3xl font-bold mb-5 text-[#03265D]">
-        Registrate para poder comprar
-      </h1>
+    <div className="px-4">
+      <div className="shadow-2xl p-5 rounded-2xl w-full max-w-[420px] mx-auto">
+        <h1 className="text-xl md:text-3xl font-bold mb-5 text-[#03265D]">
+          Registrate para comenzar
+        </h1>
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-4 max-w-md mx-auto"
-      >
-        {/* Nombre */}
-        <div>
-          <input
-            type="text"
-            placeholder="Nombre"
-            {...register("firstName", { required: "El nombre es obligatorio" })}
-            className="border p-2 rounded w-full"
-          />
-          {errors.firstName && (
-            <p className="text-red-500 text-start mt-1 text-sm font-bold flex items-center gap-1">
-              <LuTriangleAlert className="size-4" />
-              {errors.firstName.message}
-            </p>
-          )}
-        </div>
-
-        {/* Apellido */}
-        <div>
-          <input
-            type="text"
-            placeholder="Apellido"
-            {...register("lastName", {
-              required: "El apellido es obligatorio",
-            })}
-            className="border p-2 rounded w-full"
-          />
-          {errors.lastName && (
-            <p className="text-red-500 text-start mt-1 text-sm font-bold flex items-center gap-1">
-              <LuTriangleAlert className="size-4" />
-              {errors.lastName.message}
-            </p>
-          )}
-        </div>
-
-        {/* Email */}
-        <div>
-          <input
-            type="email"
-            placeholder="Email"
-            {...register("email", {
-              required: "El email es obligatorio",
-              pattern: {
-                value: /^\S+@\S+\.\S+$/,
-                message: "Email inválido",
-              },
-            })}
-            className="border p-2 rounded w-full"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-start mt-1 text-sm font-bold flex items-center gap-1">
-              <LuTriangleAlert className="size-4" />
-              {errors.email.message}
-            </p>
-          )}
-        </div>
-
-        {/* Contraseña */}
-        <div className="relative min-h-[70px]">
-          <div className="relative">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4 w-full"
+        >
+          {/* Nombre */}
+          <div>
             <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Contraseña"
-              {...register("password", {
-                required: "La contraseña es obligatoria",
-                minLength: {
-                  value: 6,
-                  message: "Debe tener al menos 6 caracteres",
-                },
-                pattern: {
-                  value: /^(?=.*[A-Z])(?=.*\d).+$/,
-                  message: "Incluya mayúsculas y numeros",
-                },
+              type="text"
+              placeholder="Nombre"
+              {...register("firstName", {
+                required: "El nombre es obligatorio",
               })}
-              className="border p-2 rounded w-full pr-10"
+              className="border p-2 rounded w-full"
             />
-
-            <span
-              className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-600"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
-            </span>
+            {errors.firstName && (
+              <p className="text-red-500 text-start mt-1 text-sm font-bold flex items-center gap-1">
+                <LuTriangleAlert className="size-4" />
+                {errors.firstName.message}
+              </p>
+            )}
           </div>
 
-          {errors.password && (
-            <p className="text-red-500 text-start mt-1 text-sm font-bold flex items-center gap-1">
-              <LuTriangleAlert className="size-4" />
-              {errors.password.message}
-            </p>
-          )}
-        </div>
+          {/* Apellido */}
+          <div>
+            <input
+              type="text"
+              placeholder="Apellido"
+              {...register("lastName", {
+                required: "El apellido es obligatorio",
+              })}
+              className="border p-2 rounded w-full"
+            />
+            {errors.lastName && (
+              <p className="text-red-500 text-start mt-1 text-sm font-bold flex items-center gap-1">
+                <LuTriangleAlert className="size-4" />
+                {errors.lastName.message}
+              </p>
+            )}
+          </div>
 
-        <button
-          type="submit"
-          disabled={!isValid}
-          className={`font-bold p-2 rounded transition-colors 
-    ${
-      isValid
-        ? "bg-[#03265D] text-white cursor-pointer hover:bg-[#021a40]"
-        : "bg-gray-400 text-gray-200 cursor-not-allowed"
-    }`}
-        >
-          Registrarse
-        </button>
+          {/* Email */}
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              {...register("email", {
+                required: "El email es obligatorio",
+                pattern: {
+                  value: /^\S+@\S+\.\S+$/,
+                  message: "Email inválido",
+                },
+              })}
+              className="border p-2 rounded w-full"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-start mt-1 text-sm font-bold flex items-center gap-1">
+                <LuTriangleAlert className="size-4" />
+                {errors.email.message}
+              </p>
+            )}
+          </div>
 
-        <div className="w-full h-px bg-[#03265D] my-4"></div>
+          {/* Contraseña */}
+          <div className="relative min-h-[70px]">
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Contraseña"
+                {...register("password", {
+                  required: "La contraseña es obligatoria",
+                  minLength: {
+                    value: 6,
+                    message: "Debe tener al menos 6 caracteres",
+                  },
+                  pattern: {
+                    value: /^(?=.*[A-Z])(?=.*\d).+$/,
+                    message: "Incluya mayúsculas y números",
+                  },
+                })}
+                className="border p-2 rounded w-full pr-10"
+              />
 
-        <p className="text-gray-700 text-center">
-          ¿Ya tenés cuenta?{" "}
-          <Link
-            to="/login"
-            className="text-[#03265D] font-semibold hover:underline"
+              <span
+                className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-600"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
+
+            {errors.password && (
+              <p className="text-red-500 text-start mt-1 text-sm font-bold flex items-center gap-1">
+                <LuTriangleAlert className="size-4" />
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            disabled={!isValid}
+            className={`font-bold p-2 rounded transition-colors 
+      ${
+        isValid
+          ? "bg-[#03265D] text-white cursor-pointer hover:bg-[#021a40]"
+          : "bg-gray-400 text-gray-200 cursor-not-allowed"
+      }`}
           >
-            Iniciá sesión
-          </Link>
-        </p>
-      </form>
+            Registrarse
+          </button>
+
+          <div className="w-full h-px bg-[#03265D] my-4"></div>
+
+          <p className="text-gray-700 text-center">
+            ¿Ya tenés cuenta?{" "}
+            <Link
+              to="/login"
+              className="text-[#03265D] font-semibold hover:underline"
+            >
+              Iniciá sesión
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
