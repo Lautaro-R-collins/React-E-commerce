f1baa3a0b01da5730c71d5b2382dce2292636279
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-06 19:54:02 -0300

    add - home components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,33 @@
 import { useProduct } from "../context/productContext.jsx";
+import SliderHome from "../components/home/CarruselImg.jsx";
+import FeaturedProducts from "../components/home/FeaturedProducts.jsx";
+import CategoriesHome from "../components/home/CategoriesHome.jsx";
 import CardProduct from "../components/products/CardProduct.jsx";
 
 export const Home = () => {
   const { products, loading, error } = useProduct();
 
-  // Loading
-  if (loading) {
+  if (loading)
     return (
       <div className="text-center mt-10 text-xl">Cargando productos...</div>
     );
-  }
-
-  // Error
-  if (error) {
+  if (error)
     return (
       <div className="text-center mt-10 text-red-500 font-semibold">
         {error}
       </div>
     );
-  }
 
   return (
-    <div className="px-4">
+    <div>
+      <SliderHome />
+      <FeaturedProducts />
+      <CategoriesHome />
       <h1 className="text-3xl text-center font-bold my-6">
         Bienvenido a la tienda
       </h1>
 
-      {/* GRID de productos */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 place-items-center px-4 md:px-20">
         {products.map((product) => (
           <CardProduct key={product._id} product={product} />
         ))}
