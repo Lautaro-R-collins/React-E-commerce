316bbb1c36bccd91c14f1fd16c3525d7feba3e79
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-08 17:50:08 -0300

    add - style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../src/layout/layout.jsx";
+
+import "./App.css";
+
+// context
+import { CartProvider } from "./context/CartContext.jsx";
+import { FavoritesProvider } from "./context/FavoritesContext";
 import { UserProvider } from "./context/userContext.jsx";
 import { ProductProvider } from "./context/productContext.jsx";
-import { CartProvider } from "./context/CartContext.jsx";
-import "./App.css";
 
 // rutas pages
 import { Home } from "./pages/Home.jsx";
@@ -14,30 +18,34 @@ import CategoryPage from "./pages/CategoryPage.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import SearchResults from "./pages/SearchResults.jsx";
 import CheckoutForm from "./pages/CheckoutForm.jsx";
+import FavoritesPage from "./pages/FavoritesPage.jsx";
 
 function App() {
   return (
-    <UserProvider>
-      <ProductProvider>
-        <CartProvider>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-              <Route path="product/:id" element={<DetailProduct />} />
-              <Route
-                path="/category/:category/:subcategory?"
-                element={<CategoryPage />}
-              />
-              <Route path="/search" element={<SearchResults />} />
-              <Route path="/checkout" element={<CheckoutForm />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </CartProvider>
-      </ProductProvider>
-    </UserProvider>
+    <FavoritesProvider>
+      <UserProvider>
+        <ProductProvider>
+          <CartProvider>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="product/:id" element={<DetailProduct />} />
+                <Route
+                  path="/category/:category/:subcategory?"
+                  element={<CategoryPage />}
+                />
+                <Route path="/search" element={<SearchResults />} />
+                <Route path="/checkout" element={<CheckoutForm />} />
+                <Route path="/favorites" element={<FavoritesPage />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </CartProvider>
+        </ProductProvider>
+      </UserProvider>
+    </FavoritesProvider>
   );
 }
 
