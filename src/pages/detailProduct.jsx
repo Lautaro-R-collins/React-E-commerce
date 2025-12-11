bd3468f6199987f3991b023628fd6ca0e17d668c
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-11 20:46:16 -0300

    ref - detailProduct

--- src/pages/detailProduct.jsx
+++ src/pages/detailProduct.jsx
@@ -112,7 +112,28 @@ export const DetailProduct = () => {
         {/* ==== INFO PRODUCTO ==== */}
         <div className="lg:w-1/2 flex flex-col gap-4">
           <p className="text-gray-700 text-lg">{product.description}</p>
-          <p className="text-3xl font-bold">${product.price}</p>
+          <div className="flex items-center gap-3">
+            {product.discountActive ? (
+              <>
+                <p className="text-2xl text-gray-500 line-through font-semibold">
+                  ${product.price}
+                </p>
+                <p className="text-3xl font-bold text-green-600">
+                  $
+                  {Math.round(
+                    product.price - (product.price * product.discount) / 100
+                  )}
+                </p>
+              </>
+            ) : (
+              <p className="text-3xl font-bold">${product.price}</p>
+            )}
+            {product.discountActive && (
+              <p className="text-red-600 font-semibold -mt-3 relative">
+                {product.discount}% OFF
+              </p>
+            )}
+          </div>
 
           <p className="text-green-600 font-semibold">
             Stock disponible: {product.stock}
