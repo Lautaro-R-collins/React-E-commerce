541b32bd3e57ab71ff50e0b462c73e0ac7089268
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-11 20:46:28 -0300

    ref - card Product

--- src/components/products/CardProduct.jsx
+++ src/components/products/CardProduct.jsx
@@ -36,7 +36,31 @@ const CardProduct = ({ product }) => {
         <div className="p-4 flex flex-col gap-2 flex-1">
           <h3 className="text-lg font-semibold text-black">{product.name}</h3>
 
-          <p className="text-black font-bold text-lg">${product.price}</p>
+          <div className="flex items-center gap-2">
+            {product.discountActive ? (
+              <>
+                <span className="text-gray-400 line-through font-semibold">
+                  ${product.price}
+                </span>
+
+                <span className="text-green-600 font-bold">
+                  $
+                  {Math.round(
+                    product.price - (product.price * product.discount) / 100
+                  )}
+                </span>
+              </>
+            ) : (
+              <span className="text-black font-bold text-lg">
+                ${product.price}
+              </span>
+            )}
+            {product.discountActive && (
+              <p className="text-red-600 font-semibold text-xs -mt-3 relative">
+                {product.discount}% OFF
+              </p>
+            )}
+          </div>
 
           <p className="text-gray-600 text-sm">Stock: {product.stock}</p>
         </div>
