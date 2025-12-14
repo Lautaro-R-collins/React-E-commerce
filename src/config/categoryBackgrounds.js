840f8cbe4d7a40c0e97e1f953de5063e689473f8
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-14 07:55:33 -0300

    add category bg img

---
+++ src/config/categoryBackgrounds.js
@@ -0,0 +1,11 @@
+
+export const CATEGORY_BACKGROUNDS = {
+  ropa: "/categories/ropa.webp",
+  tecnologia: "/categories/Tecnologias.webp",
+  hogar: "/categories/Hogar.png",
+  electrodomesticos: "/categories/electro.jpg",
+  ofertas: "/categories/ofertas.png",
+  favoritos: "/categories/favoritos.png",
+
+  default: "/categories/default.jpg",
+};
