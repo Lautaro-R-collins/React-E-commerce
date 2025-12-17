cb486094c8f6982ce1efc67bc1b3b6be197aba86
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-17 03:32:04 -0300

    add dashboard admin layout

---
+++ src/layout/DashboardLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import {Footer} from "../components/footer/footer.jsx";
+import NavBar from "../components/navBar/navBar.jsx";
+
+const DashboardLayout = () => {
+  return (
+        <div className="min-h-screen flex flex-col">
+          <NavBar />
+          <main className="justify-center items-start">
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+  );
+};
+
+export default DashboardLayout;
