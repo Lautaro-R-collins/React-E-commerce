1a8396098653c6fc68b4eea0b838f95ff2a0e068
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-18 14:41:59 -0300

    refactor -  login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "../components/login/loginForm.jsx";
+
+const Login = () => {
+  return (
+    <div className="text-center my-10">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
