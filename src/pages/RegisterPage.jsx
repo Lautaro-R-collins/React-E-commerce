d4cfd8a970875a6f4495160804785d9cf7a8f7fd
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-18 14:41:24 -0300

    refactor -  regsiterPage

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../components/register/registerForm.jsx";
+
+const Register = () => {
+  return (
+    <div className=" text-center my-10">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
