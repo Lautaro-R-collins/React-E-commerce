d8ec7e13d38da052785a633a868bdf2421309725
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-18 14:41:32 -0300

    refactor -  features

--- src/components/home/BackFeatures.jsx
+++ src/components/home/BackFeatures.jsx
@@ -1,34 +1,11 @@
-import { FaTruck, FaRegCreditCard, FaShieldAlt } from "react-icons/fa";
+// src/components/home/Features.jsx
 import CardFeature from "./CardFeature.jsx";
+import { homeFeatures } from "../../mocks/homeFeatures.mock";
 
-const featuresData = [
-  {
-    id: 1,
-    icon: FaRegCreditCard,
-    title: "Elegí cómo pagar",
-    description:
-      "Podés pagar con tarjeta, débito, efectivo o con Cuotas sin Tarjeta.",
-  },
-  {
-    id: 2,
-    icon: FaTruck,
-    title: "Envío gratis",
-    description:
-      "Tenes envio sin costo en todos nuestros productos a partir de 5.000$",
-  },
-  {
-    id: 3,
-    icon: FaShieldAlt,
-    title: "Seguridad",
-    description:
-      "No hay nada que no puedas hacer, porque estás siempre protegido.",
-  },
-];
-
-export const Features = () => {
+const Features = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-10 px-6  sm:flex-row sm:flex-wrap sm:gap-12 lg:justify-center">
-      {featuresData.map((feature) => (
+    <div className="flex flex-col items-center justify-center gap-10 px-6 sm:flex-row sm:flex-wrap sm:gap-12 lg:justify-center">
+      {homeFeatures.map((feature) => (
         <CardFeature
           key={feature.id}
           icon={feature.icon}
