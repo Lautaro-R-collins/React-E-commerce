84679801174c8343b7bd45fa8aee0753ea8fbc5b
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-18 14:39:09 -0300

    add homeFeatures mock

---
+++ src/mocks/homeFeatures.mock.js
@@ -0,0 +1,25 @@
+// src/mocks/homeFeatures.mock.js
+import { FaTruck, FaRegCreditCard, FaShieldAlt } from "react-icons/fa";
+
+export const homeFeatures = [
+  {
+    id: 1,
+    icon: FaRegCreditCard,
+    title: "Elegí cómo pagar",
+    description:
+      "Podés pagar con tarjeta, débito, efectivo o con cuotas sin tarjeta.",
+  },
+  {
+    id: 2,
+    icon: FaTruck,
+    title: "Envío gratis",
+    description:
+      "Tenés envío sin costo en todos nuestros productos a partir de $5.000.",
+  },
+  {
+    id: 3,
+    icon: FaShieldAlt,
+    title: "Seguridad",
+    description: "Tus compras están protegidas en todo momento.",
+  },
+];
