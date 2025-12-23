948d0e2590e6099010abb3b5d1ad73be90d195a1
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-23 12:53:32 -0300

    refactor hook context

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -13,7 +13,7 @@ export const UserProvider = ({ children }) => {
 
       const userData = await getProfileService();
       setUserInfo(userData); 
-    } catch (error) {
+    } catch {
       setUserInfo({});
     } finally {
       setLoading(false);
