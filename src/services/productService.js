0532987d28d8b727937576d4fb629ba3918ca197
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-23 12:53:54 -0300

    test

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,10 +1,6 @@
 import api from "../config/api";
 
 export const getProductsService = async (params) => {
-    try {
-        const response = await api.get("/products", { params });
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+    const response = await api.get("/products", { params });
+    return response.data;
 };
