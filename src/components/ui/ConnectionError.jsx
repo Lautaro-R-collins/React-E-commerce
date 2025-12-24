5ca5c4bbdb008e5734b36b7e6d3429a43e69cfd3
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-24 11:05:45 -0300

    refactor - component - Error Connection

--- src/components/ui/ConnectionError.jsx
+++ src/components/ui/ConnectionError.jsx
@@ -5,12 +5,11 @@ const ConnectionError = ({ onRetry }) => {
     <div className="flex flex-col items-center justify-center text-center py-20 px-4">
       <LuWifiOff className="text-[200px] text-gray-400 mb-4" />
 
-      <h2 className="text-2xl font-bold mb-2">
-        Sin conexión a internet
-      </h2>
+      <h2 className="text-2xl font-bold mb-2">Sin conexión a internet</h2>
 
       <p className="text-gray-500 max-w-md mb-6">
-        No pudimos cargar los productos. Verificá tu conexión a internet y volvé a intentarlo.
+        No pudimos cargar los productos. Verificá tu conexión a internet y volvé
+        a intentarlo.
       </p>
 
       {onRetry && (
